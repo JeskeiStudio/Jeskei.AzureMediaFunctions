95a0d9460c0448f46f51efd84b38458f8dd3b7ac
TheNewAutonomy <darren.op@cent.finance>
2022-07-10 00:16:12 +0100

    change access to anonymous for testing

--- JeskeiMediaFunctions/CreateEmptyAsset.cs
+++ JeskeiMediaFunctions/CreateEmptyAsset.cs
@@ -76,7 +76,7 @@ namespace JeskeiMediaFunctions
 
         [FunctionName("CreateEmptyAsset")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
